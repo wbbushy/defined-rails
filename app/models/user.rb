a7f1a40dfb68b39a2345b018139f9c646e1e5fdf
Wm. Butler Bushyhead <Butler@Williams-MacBook-Pro.local>
2015-02-10 14:15:22 -0800

    Removed attr_accesible as it is no longer supported in this version of rails Added BCrpyt Added password methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :user_name, :password
 
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
 
 end
