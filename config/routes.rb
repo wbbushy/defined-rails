a5138789561dcc7656dd3fce228b5a01dd726e7f
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-02-13 16:10:02 -0800

    Added sessions resources Added delete route for logout linked to controller sessions#destroy added get main to pages#main

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
    root to: 'pages#main'
 
   resources :users
+  resources :sessions, only: [:create]
+  delete "logout" => "sessions#destroy"
+
+  get '/main', to: 'pages#main', as: 'main'
 
 end
