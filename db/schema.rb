4d5756af4984f0eb7e00130ca46983b0d1583bc5
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-02-17 13:31:21 -0800

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128230903) do
+ActiveRecord::Schema.define(version: 20150217002853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,19 @@ ActiveRecord::Schema.define(version: 20150128230903) do
     t.string   "user_name"
     t.string   "email"
     t.string   "password_hash"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users_words", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "words", force: true do |t|
+    t.integer  "user_id"
+    t.string   "spelling"
+    t.string   "definition"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
