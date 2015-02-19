9f30bae7a1bda704b9e75ae911fabdf7bf70b513
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-02-19 13:29:02 -0800

    Added method define that hit's Meriam Webster API to retrieve XML extract defintion from XML run define method in the word create method. Save defintion to word in it's creation

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,8 +1,25 @@
 class WordsController < ApplicationController
 
+  require 'open-uri'
+  require 'nokogiri'
+
+
+  def define(word)
+    @url = "http://www.dictionaryapi.com/api/v1/references/collegiate/xml/" + word.spelling.to_s + "?key=" + ENV["API_KEY"]
+    @xml_doc = Nokogiri::XML(open(@url))
+    @definitions = @xml_doc.xpath("//dt")
+    @definitionOne = @definitions[0].to_s[4..-6]
+    @definitionOne = @definitionOne.gsub(/(?i)<vi[^>]*>/, "\"")
+    @definitionOne = @definitionOne.gsub(/(?i)<\/vi[^>]*>/, "\"")
+    @definitionOne = @definitionOne.gsub(/<[^>]*>/,"")
+  end
+
+
   def create
     @word = Word.new(spelling: params["spelling"])
     @word.user_id = session[:id]
+    define(@word)
+    @word.definition = @definitionOne
     @word.save
     redirect_to main_path
   end
