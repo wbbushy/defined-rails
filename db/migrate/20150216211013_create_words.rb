41b85df8608bf50cb0e0db52ac369696b6bb4350
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-02-20 16:07:37 -0800

    Fixed migration. Removed belongs to user association

--- db/migrate/20150216211013_create_words.rb
+++ db/migrate/20150216211013_create_words.rb
@@ -1,7 +1,6 @@
 class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
-      t.belongs_to :user
       t.string :spelling
       t.string :definition
       t.timestamps
