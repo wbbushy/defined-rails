a697aa0d3223161a2f78f914aef006342a23c15f
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-02-20 16:08:24 -0800

    Added association belongs to word and belongs to user

--- db/migrate/20150217002853_create_users_words.rb
+++ db/migrate/20150217002853_create_users_words.rb
@@ -1,6 +1,8 @@
 class CreateUsersWords < ActiveRecord::Migration
   def change
     create_table :users_words do |t|
+      t.belongs_to :user
+      t.belongs_to :word
       t.timestamps
     end
   end
