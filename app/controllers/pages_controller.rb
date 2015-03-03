f6189890173155d09d80b4c4bd49af7eda6ea844
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-03-03 13:49:02 -0800

    fixed stash conflict

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,16 +2,8 @@ class PagesController < ApplicationController
 
   def main
     @user = User.new
-<<<<<<< Updated upstream
-    @words = UsersWord.where(user_id: session[:id])
-=======
-<<<<<<< Updated upstream
-    @words = Word.all
-=======
     @words = UsersWord.where(user_id: session[:id])
     @current_user ||= User.find(session[:id]) if session[:id]
->>>>>>> Stashed changes
->>>>>>> Stashed changes
   end
 
 end
