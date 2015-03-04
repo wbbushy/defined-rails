91618cb83e4562a2471d41cb6f0ae617bf0d44bc
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-03-04 10:03:35 -0800

    changed definition to be a text value instead of a string

--- db/migrate/20150216211013_create_words.rb
+++ db/migrate/20150216211013_create_words.rb
@@ -2,7 +2,7 @@ class CreateWords < ActiveRecord::Migration
   def change
     create_table :words do |t|
       t.string :spelling
-      t.string :definition
+      t.text :definition
       t.timestamps
     end
   end
