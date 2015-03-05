2f207af25563f2f5766e2feeede8f76980697af5
Wm. Butler Bushyhead <wbbushyhead@gmail.com>
2015-03-05 14:55:50 -0800

    Added params for remember me in pages main Added method dashboard with @user, @current_user, @words and params remember me

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,14 @@ class PagesController < ApplicationController
     @user = User.new
     @words = UsersWord.where(user_id: session[:id])
     @current_user ||= User.find(session[:id]) if session[:id]
+    params[:remember_me] = true
+  end
+
+  def dashboard
+    @user = User.new
+    @words = UsersWord.where(user_id: session[:id])
+    @current_user ||= User.find(session[:id]) if session[:id]
+    params[:remember_me] = true
   end
 
 end
